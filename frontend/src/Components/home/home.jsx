09c51bd344b94db253295b6b0fee48608d3c2bfe
손은정 <son-eunjung@son-eunjung-ui-MacBookPro.local>
2022-02-12 12:15:13 +0900

    add  로그인 로그아웃 바뀌는 이벤트

--- frontend/src/Components/home/home.jsx
+++ frontend/src/Components/home/home.jsx
@@ -25,32 +25,15 @@ const HomeBlock = styled.div`
   position: relative;
 `;
 const Home = () => {
-  // const user = localStorage.getItem("user");
-  const navigate = useNavigate();
-
-  //authCheck === true -> 로그인 상태
-  //authCheck === false -> 로그아웃 상태
-  const authCheck = useRecoilValue(authAtom);
-
-  const userActions = useUserActions();
-
   return (
     <HomeBlock>
       <TopNav_main />
       <Slogan></Slogan>
-      {/* <FamousView></FamousView> */}
-      {/* <FamousViewWithOneSlider></FamousViewWithOneSlider> */}
-      {/* <FamousViewWithSlider></FamousViewWithSlider> */}
-      {/* <FamousView></FamousView> */}
-      {/* <ButtonWrapper>
-        {!authCheck && <StyledBtn onClick={() => navigate("/login")}>login</StyledBtn>}
-        {authCheck && <StyledBtn onClick={userActions.logout}>logout</StyledBtn>}
-        <StyledBtn onClick={() => navigate("/users/mypage")}>mypage</StyledBtn>
-        <StyledBtn onClick={() => navigate("post")}>post</StyledBtn>
-        <StyledBtn onClick={() => navigate("/view_all")}>전체글 보기</StyledBtn>
-      </ButtonWrapper> */}
+
+      <FamousViewWithOneSlider></FamousViewWithOneSlider>
+
       <SloganBottom></SloganBottom>
-      <BottomNav />
+      <BottomNav></BottomNav>
     </HomeBlock>
   );
 };
